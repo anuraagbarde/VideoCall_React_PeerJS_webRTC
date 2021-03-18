f58db87c5d469fedb68151b0d7a08ea7570942a6
Anuraag Barde <anuraagbarde@gmail.com>
2021-03-18 17:37:43 +0530

    removed ssl in peerserver.js and delte .env for react

--- frontend/peerServer.js
+++ frontend/peerServer.js
@@ -11,9 +11,9 @@ const PEERJSSERVERPATH = '/peerserver';
 const peerServer = PeerServer({ 
     port: PORT,
     path: PEERJSSERVERPATH,
-    ssl: {
-        key: fs.readFileSync('../.localCert/keyClientLocal_192_168_15_169.pem'),
-        cert: fs.readFileSync('../.localCert/certClientLocal_192_168_15_169.pem')
-    }    
+    // ssl: {
+    //     key: fs.readFileSync('../.localCert/keyClientLocal_192_168_15_169.pem'),
+    //     cert: fs.readFileSync('../.localCert/certClientLocal_192_168_15_169.pem')
+    // }    
 });
 console.log('PeerJS server running on port ' + PORT)
